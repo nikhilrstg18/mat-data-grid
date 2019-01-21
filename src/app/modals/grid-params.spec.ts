9879cd30c3d8fbace2917357451d7e8d3e69fad5
Nikhil Rustagi <nikhil_rustagi@dellteam.com>
2019-01-21 11:28:48 +0530

    initial commit- added mat-data-grid component|service|modals

---
+++ src/app/modals/grid-params.spec.ts
@@ -0,0 +1,7 @@
+import { GridParams } from './grid-params';
+
+describe('GridParams', () => {
+  it('should create an instance', () => {
+    expect(new GridParams()).toBeTruthy();
+  });
+});
---
+++ src/app/modals/mat-data-grid-col-def.spec.ts
@@ -0,0 +1,7 @@
+import { MatDataGridColDef } from './mat-data-grid-col-def';
+
+describe('MatDataGridColDef', () => {
+  it('should create an instance', () => {
+    expect(new MatDataGridColDef()).toBeTruthy();
+  });
+});
