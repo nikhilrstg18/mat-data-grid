c0b268dfed461df0f8765d0f806fb52f57f892b7
Nikhil Rustagi <nikhil_rustagi@dellteam.com>
2019-01-26 13:35:27 +0530

    minor fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,15 +15,15 @@ export const environment = {
   img:{
     infiniteScrolling: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/infinitescroll.PNG',
     sorting:{
-      s1: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s1.PNG',
-      s2: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s2.PNG',
-      s3: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s3.PNG',
-      s4: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s4.PNG',
-      s5: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s5.PNG',
+      s1: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s1.png',
+      s2: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s2.png',
+      s3: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s3.png',
+      s4: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s4.png',
+      s5: 'https://nikhilrstg18.github.io/mat-data-grid/assets/img/s5.png',
     },
     filtering:{
-      f1:'https://nikhilrstg18.github.io/mat-data-grid/assets/img/f1.PNG',
-      f2:'https://nikhilrstg18.github.io/mat-data-grid/assets/img/f2.PNG',
+      f1:'https://nikhilrstg18.github.io/mat-data-grid/assets/img/f1.png',
+      f2:'https://nikhilrstg18.github.io/mat-data-grid/assets/img/f2.png',
     }
   }
 };
